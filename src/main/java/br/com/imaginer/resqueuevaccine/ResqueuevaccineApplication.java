cccdc43cf09002de17c1f4c494578c33cf687a10
Thiago Pereira <thiago.pereira7ps@gmail.com>
2025-02-18 14:42:26 -0300

    Add existing project files to Git

---
+++ src/main/java/br/com/imaginer/resqueuevaccine/ResqueuevaccineApplication.java
@@ -0,0 +1,13 @@
+package br.com.imaginer.resqueuevaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResqueuevaccineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResqueuevaccineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/imaginer/resqueuevaccine/ResqueuevaccineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imaginer.resqueuevaccine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResqueuevaccineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
