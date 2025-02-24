833e69cba708e818b4dadef0504ab3772e5cdc02
rodrigobrocchi <rodrigo.brocchi@gmail.com>
2025-02-24 04:30:08 -0300

    Refactor `FeignClientInterceptor` to ensure the authorization header is correctly formatted before adding it to the request template.

--- src/main/java/br/com/imaginer/resqueuevaccine/config/FeignClientInterceptor.java
+++ src/main/java/br/com/imaginer/resqueuevaccine/config/FeignClientInterceptor.java
@@ -19,11 +19,10 @@ public class FeignClientInterceptor implements RequestInterceptor {
       HttpServletRequest request = attributes.getRequest();
       String token = request.getHeader(AUTHORIZATION_HEADER);
 
-      if (token != null && !token.startsWith(BEARER_TOKEN_PREFIX)) {
-        token = BEARER_TOKEN_PREFIX + token;
-      }
-
       if (token != null) {
+        if (!token.startsWith(BEARER_TOKEN_PREFIX)) {
+          token = BEARER_TOKEN_PREFIX + token;
+        }
         requestTemplate.header(AUTHORIZATION_HEADER, token);
       }
     }
