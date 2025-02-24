69a5d73f0d533a1b0a03c8fa9129cd766bd55d67
rodrigobrocchi <rodrigo.brocchi@gmail.com>
2025-02-24 04:12:24 -0300

    Add Feign client configuration and dependencies

--- src/main/java/br/com/imaginer/resqueuevaccine/ResqueuevaccineApplication.java
+++ src/main/java/br/com/imaginer/resqueuevaccine/ResqueuevaccineApplication.java
@@ -2,10 +2,12 @@ package br.com.imaginer.resqueuevaccine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 public class ResqueuevaccineApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/imaginer/resqueuevaccine/config/FeignClientInterceptor.java
@@ -0,0 +1,31 @@
+package br.com.imaginer.resqueuevaccine.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+  private static final String AUTHORIZATION_HEADER = "Authorization";
+  private static final String BEARER_TOKEN_PREFIX = "Bearer ";
+
+  @Override
+  public void apply(RequestTemplate requestTemplate) {
+    ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    if (attributes != null) {
+      HttpServletRequest request = attributes.getRequest();
+      String token = request.getHeader(AUTHORIZATION_HEADER);
+
+      if (token != null && !token.startsWith(BEARER_TOKEN_PREFIX)) {
+        token = BEARER_TOKEN_PREFIX + token;
+      }
+
+      if (token != null) {
+        requestTemplate.header(AUTHORIZATION_HEADER, token);
+      }
+    }
+  }
+}
